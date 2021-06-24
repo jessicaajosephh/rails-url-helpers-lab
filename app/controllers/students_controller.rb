4cf935dbb8f43598ecbcf3fa9ce2d0d328bde682
Jessica Joseph <jessicaajosephh@gmail.com>
2021-06-24 14:08:15 -0400

    commit tree 1b0738e7efffe21b1018e6e3677310d7349100ab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,17 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: [:show, :update]
   
   def index
     @students = Student.all
   end
 
   def show
+    #@student = Student.find_by(params[:id])
+  end
+
+  def update 
+    @student.toggle!(:active)
+    redirect_to student_path(@student)
   end
 
   private
